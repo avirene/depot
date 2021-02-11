682f41eb5dbe6e24a1ee5fe88f5e9eedcbe2d07d
Iryna Audzei <avirene@live.com>
2021-02-11 14:55:43 +0000

    Added tests

--- test/models/product_test.rb
+++ test/models/product_test.rb
@@ -1,6 +1,7 @@
 require 'test_helper'
 
 class ProductTest < ActiveSupport::TestCase
+  fixtures :products
   test "product attributes must not be empty" do
     product = Product.new
     assert product.invalid?
@@ -22,6 +23,12 @@ class ProductTest < ActiveSupport::TestCase
 
     product.price = 1
     assert product.valid?
+  end
+
+  test "product is not valid without a unique title - i18n" do
+    product = Product.new(title: products(:ruby).title, description: "yyy", price: 1, image_url: fred.gif)
+    assert product.invalid?
+    assert_equal [I18n.translate('errors.messages.taken')], product_errors[:title]
   end
 
   def new_product(image_url)
