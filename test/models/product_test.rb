6755575623df8da299e732ce9dfc42d997fc5148
Iryna Audzei <avirene@live.com>
2021-02-12 14:17:48 +0000

    Added fragment caching for portions of the catalog

--- test/models/product_test.rb
+++ test/models/product_test.rb
@@ -26,9 +26,9 @@ class ProductTest < ActiveSupport::TestCase
   end
 
   test "product is not valid without a unique title - i18n" do
-    product = Product.new(title: products(:ruby).title, description: "yyy", price: 1, image_url: fred.gif)
+    product = Product.new(title: products(:ruby).title, description: "yyy", price: 1, image_url: "fred.gif")
     assert product.invalid?
-    assert_equal [I18n.translate('errors.messages.taken')], product_errors[:title]
+    assert_equal [I18n.translate('errors.messages.taken')], product.errors[:title]
   end
 
   def new_product(image_url)
