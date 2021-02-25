72e665c75b45d8ba3022c4d0d4ea16277f0dd83e
Iryna Audzei <avirene@live.com>
2021-02-25 17:08:44 +0000

    Changed the configuration to include lib in the load path

--- config/application.rb
+++ config/application.rb
@@ -13,6 +13,8 @@ module Depot
 
     config.middleware.use I18n::JS::Middleware
 
+    config.autoload_paths += %W(#{Rails.root}/lib)
+
     # Settings in config/environments/* take precedence over those specified here.
     # Application configuration can go into files in config/initializers
     # -- all .rb files in that directory are automatically loaded after loading
