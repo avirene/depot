57e465d0d3cb9d7c4d26923aa967b4f6059d2342
Iryna Audzei <avirene@live.com>
2021-03-02 12:25:56 +0000

    Added email validation for @

--- app/models/order.rb
+++ app/models/order.rb
@@ -11,6 +11,7 @@ class Order < ApplicationRecord
 
   validates :name, :address, :email, presence: true
   validates :pay_type, inclusion: pay_types.keys
+  validates :email, format: { with: URI::MailTo::EMAIL_REGEXP }
 
   def add_line_items_from_cart(cart)
     cart.line_items.each do |item|
